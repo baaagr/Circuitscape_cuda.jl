b335e5078371ed47499dae07ff8964262ab5e331
baaagr <bartoszgren@gmail.com>
2025-03-26 13:12:36 +0000

    ...

--- src/raster/advanced.jl
+++ src/raster/advanced.jl
@@ -311,7 +311,7 @@ function multiple_solver(cfg, solver, a::SparseMatrixCSC{T,V}, sources, grounds,
 end
 
 function multiple_solve(s::AMGSolver, matrix::CUSPARSE.CuSparseMatrixCSC{T,V}, sources::CuVector{T}, suppress_info::Bool) where {T,V}
-    t1 = @elapsed M = aspreconditioner(smoothed_aggregation(matrix))
+    t1 = @elapsed M = matrix
     csinfo("Time taken to construct preconditioner = $t1 seconds", suppress_info)
     t1 = @elapsed volt_gpu = solve_linear_system(matrix, sources, M)
     # @assert norm(matrix*volt .- sources) < (eltype(sources) == Float64 ? TOL_DOUBLE : TOL_SINGLE)
