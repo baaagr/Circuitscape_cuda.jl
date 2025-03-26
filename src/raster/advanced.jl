64dfa53498f575846294abe0681b12a5a637adf8
baaagr <bartoszgren@gmail.com>
2025-03-26 12:53:22 +0000

    Resteg still..

--- src/raster/advanced.jl
+++ src/raster/advanced.jl
@@ -311,7 +311,7 @@ function multiple_solver(cfg, solver, a::SparseMatrixCSC{T,V}, sources, grounds,
 end
 
 function multiple_solve(s::AMGSolver, matrix::CUSPARSE.CuSparseMatrixCSC{T,V}, sources::CuVector{T}, suppress_info::Bool) where {T,V}
-    t1 = @elapsed volt_gpu = solve_linear_system(matrix, sources)
+    t1 = @elapsed volt_gpu = solve_linear_system(matrix, sources, M)
     # @assert norm(matrix*volt .- sources) < (eltype(sources) == Float64 ? TOL_DOUBLE : TOL_SINGLE)
 	@assert (norm(matrix*volt_gpu .- sources) / norm(sources)) < 1e-4
     csinfo("Time taken to solve linear system = $t1 seconds", suppress_info)
